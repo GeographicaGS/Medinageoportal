8aafe90602d0ad66fddd88848ec412767fef416e
Alberto Asuero <alberto.asuero@gstgis.com>
2013-02-06 12:27:45 +0100

    change layers

--- www/js/layers.js
+++ www/js/layers.js
@@ -1,45 +1,126 @@
 var layers = [
+
 	{
 		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
-		title: "T-Sea-Surface-Temperature",
-		layers: "T-Sea-Surface-Temperature",
-		priority: 6,
-		visible: false,
-	},
-	{
-		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
-		title: "Marine_ecoregions",
+		title: "Marine Ecoregions",
 		layers: "Marine_ecoregions",
-		priority: 5,
-		visible: false,		
-	},
-	{
-		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
-		title: "GLOBCORINE_2009",
-		layers: "GLOBCORINE_2009",
 		priority: 4,
 		visible: false,		
 	},
 	{
 		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
-		title: "A-Chla-Anomalies",
+		title: "Temperature in Winter for the European Marine Waters",
+		layers: "t-winter",
+		priority: 5,
+		visible: false,		
+	},
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Temperature in Summer for the European Marine Waters",
+		layers: "t-summer",
+		priority: 5,
+		visible: false,		
+	},
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Salinity in Winter for the European Marine Waters",
+		layers: "salinity-winter",
+		priority: 5,
+		visible: false,		
+	},
+	
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Salinity in Summer for the European Marine Waters",
+		layers: "salinity-summer",
+		priority: 5,
+		visible: false,		
+	},
+
+	/*{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "MODIS T Sea Surface Temperature",
+		layers: "T-Sea-Surface-Temperature",
+		priority: 5,
+		visible: false,
+	},
+	
+	
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "MODIS-A Chla Anomalies",
 		layers: "A-Chla-Anomalies",
-		priority: 3,
+		priority: 5,
+		visible: false,		
+	},*/
+	
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Glob Corine 2009",
+		layers: "GLOBCORINE_2009",
+		priority: 5,
+		visible: false,		
+	},
+
+	/*{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Chlorophyll A",
+		layers: "Chlorophyll",
+		priority: 5,
+		visible: false,		
+	},*/
+
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Variable 1: ETW",
+		layers: "etw",
+		priority: 5,
+		visible: false,		
+	},
+
+
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Variable 2: CHL",
+		layers: "chl",
+		priority: 5,
+		visible: false,		
+	},
+
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Variable 3: Depth",
+		layers: "depth",
+		priority: 5,
+		visible: false,		
+	},
+
+
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Indicator 1 Gabes",
+		layers: "indicator1",
+		priority: 4,
+		visible: false,	
+	},
+
+
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Study Cases",
+		layers: "CASES_ZONES",
+		priority: 8,
 		visible: true,		
 	},
-	{
-		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
-		title: "Chlorophyll",
-		layers: "Chlorophyll",
-		priority: 2,
-		visible: false,		
-	},
-	{
-		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
-		title: "CASES_ZONES",
-		layers: "CASES_ZONES",
-		priority: 1,
-		visible: false,		
-	}
 	
+
+	{
+		server: "http://www.medinageoportal.eu/cgi-bin/medinageoportal",
+		title: "Natural Earth Base Map",
+		layers: "Natural_earth_HR",
+		priority: 1,
+		visible: true,
+	}
+
+
 ]
