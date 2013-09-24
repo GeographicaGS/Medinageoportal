2bb00c717e7acca4828cf53e58eab8923f6e6ade
Alberto Asuero <alberto.asuero@gstgis.com>
2013-09-24 17:59:45 +0200

    sync

--- www/js/RectangleDrawer.js
+++ www/js/RectangleDrawer.js
@@ -1,6 +1,57 @@
 RectangleDrawer = {
 	_map : null,
+	_canvas: null,
+	// Left upper coordinate
+	_lu: {"x": null,"y":null},
+	// Right lower coordinate
+	_rl: {"x": null,"y": null},
+	_getMousePos: function( evt) {
+        var rect = this._canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+    },
 	ini: function (opts){
 		this._map = opts.map;
+		// create canvas on overlay Pane
+		var $overlayPane = $(this._map.getPanes().overlayPane);
+		$overlayPane.html("<canvas id='rectangle_drawer'></canvas>");
+		var $container = $(this._map.getContainer());
+		// set the width of the overlayPane
+		$overlayPane.width($container.width());
+		this._canvas = $("#rectangle_drawer")[0];
+		
+		$("#rectangle_drawer").bind('mousemove', function(evt) {
+			evt.stopPropagation();
+	        var mousePos = RectangleDrawer._getMousePos(evt);
+	        RectangleDrawer._rl.x = mousePos.x;
+	        RectangleDrawer._rl.y = mousePos.y;
+	        //  console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+		}).bind('mousedown', function(evt) {
+			evt.stopPropagation();
+	        var mousePos = RectangleDrawer._getMousePos(evt);
+	        RectangleDrawer._lu.x = mousePos.x;
+	        RectangleDrawer._lu.y = mousePos.y;	
+	        
+	        
+		});;
+		
+		setInterval(function(){
+        	RectangleDrawer._drawRect();
+        },100)
+		
+	},
+	_drawRect: function(){
+		if (this._lu.x && this._lu.y && this._rl.x && this._rl.y)
+		{
+			var width = this._rl.x - this._lu.x;
+			var height = this._rl.y - this._lu.y;
+			var ctx=this._canvas.getContext("2d");
+			ctx.fillStyle="#00ff00";
+			ctx.fillRect(this._lu.x,this._lu.y,width,height);
+			console.log("x: " + this._lu.x + " y:" + this._lu.y + " lx:" + this._rl.x + " ly:" +this._rl.y);
+			console.log("x: " + this._lu.x + " y:" + this._lu.y + " w:" + width + " h:" + height);
+		}
 	}
 }
