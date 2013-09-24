3bf556b7d6f2ac3949db732916bb8b356408c6d6
Alberto Asuero <alberto.asuero@gstgis.com>
2013-09-24 22:05:55 +0200

    change

--- www/js/RectangleDrawer.js
+++ www/js/RectangleDrawer.js
@@ -16,11 +16,14 @@ RectangleDrawer = {
 		this._map = opts.map;
 		// create canvas on overlay Pane
 		var $overlayPane = $(this._map.getPanes().overlayPane);
-		$overlayPane.html("<canvas id='rectangle_drawer'></canvas>");
+		$overlayPane.html("<canvas id='rectangle_drawer' ></canvas>");
 		var $container = $(this._map.getContainer());
 		// set the width of the overlayPane
 		$overlayPane.width($container.width());
 		this._canvas = $("#rectangle_drawer")[0];
+		
+		$(this._canvas).attr("height",$container.height());
+		$(this._canvas).attr("width",$container.width());
 		
 		$("#rectangle_drawer").bind('mousemove', function(evt) {
 			evt.stopPropagation();
@@ -33,8 +36,8 @@ RectangleDrawer = {
 	        var mousePos = RectangleDrawer._getMousePos(evt);
 	        RectangleDrawer._lu.x = mousePos.x;
 	        RectangleDrawer._lu.y = mousePos.y;	
-	        
-	        
+	        console.log("x: " + RectangleDrawer._lu.x + " y:" + RectangleDrawer._lu.y);
+	        RectangleDrawer._drawRect();
 		});;
 		
 		setInterval(function(){
@@ -45,13 +48,23 @@ RectangleDrawer = {
 	_drawRect: function(){
 		if (this._lu.x && this._lu.y && this._rl.x && this._rl.y)
 		{
+			var ctx=this._canvas.getContext("2d");
+			
+			ctx.save();
+
+			// Use the identity matrix while clearing the canvas
+			//ctx.setTransform(1, 0, 0, 1, 0, 0);
+			ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+
+			// Restore the transform
+			ctx.restore();
+			
 			var width = this._rl.x - this._lu.x;
 			var height = this._rl.y - this._lu.y;
-			var ctx=this._canvas.getContext("2d");
+			
 			ctx.fillStyle="#00ff00";
 			ctx.fillRect(this._lu.x,this._lu.y,width,height);
-			console.log("x: " + this._lu.x + " y:" + this._lu.y + " lx:" + this._rl.x + " ly:" +this._rl.y);
-			console.log("x: " + this._lu.x + " y:" + this._lu.y + " w:" + width + " h:" + height);
+			
 		}
 	}
 }
